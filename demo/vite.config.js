a6a52467cd93dc5c81aa7baad2e470cb785c1068
Param Khodiyar <paramkhodiya1008@gmail.com>
2025-03-17 10:14:46 +0530

    Configured for GitHub Pages deployment

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Update the base URL to match your repository name
 export default defineConfig({
   plugins: [react()],
+  base: "react-counter", // Change "your-repo-name" to your actual repository name
 })
